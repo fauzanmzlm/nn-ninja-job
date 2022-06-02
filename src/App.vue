b6d579cc7453ecbe0e69e5ad12d19d8c3b30f616
fauzanmzlm <fauzanmazlam88@gmail.com>
2022-06-02 08:16:41 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
 <template>
-  <nav>
+  <div id="nav"> 
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+    <router-link :to="{ name: 'about' }">About</router-link> |
+    <router-link :to="{ name: 'jobs' }">Jobs</router-link>
+  </div>
+
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go Back</button>
+  <button @click="forward">Go Forward</button>
+
   <router-view/>
 </template>
+
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name: 'home' })
+    },
+    back() {
+      this.$router.go(-1)
+    },
+    forward() {
+      this.$router.go(1)
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -15,16 +37,22 @@
   color: #2c3e50;
 }
 
-nav {
+#nav {
   padding: 30px;
 }
 
-nav a {
+#nav a {
   font-weight: bold;
   color: #2c3e50;
 }
 
-nav a.router-link-exact-active {
+#nav a.router-link-exact-active {
   color: #42b983;
+}
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure doloremque ea necessitatibus, a culpa perferendis porro beatae vero natus pariatur possimus. Beatae quibusdam quos alias voluptatem harum placeat quod cupiditate explicabo assumenda. Modi illum rem delectus nam blanditiis magnam nostrum provident perspiciatis voluptates suscipit quidem recusandae alias voluptatibus, odit error tempora beatae totam quia non reiciendis cumque consectetur. Autem sunt vel minima minus porro sint, consequuntur provident ab tempore. Atque dolorem sit numquam deleniti molestias suscipit dolores. Autem esse aperiam saepe impedit facilis reprehenderit libero nam deleniti dicta velit, alias veniam explicabo dolore iure atque eum officiis rem ratione doloribus.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure doloremque ea necessitatibus, a culpa perferendis porro beatae vero natus pariatur possimus. Beatae quibusdam quos alias voluptatem harum placeat quod cupiditate explicabo assumenda. Modi illum rem delectus nam blanditiis magnam nostrum provident perspiciatis voluptates suscipit quidem recusandae alias voluptatibus, odit error tempora beatae totam quia non reiciendis cumque consectetur. Autem sunt vel minima minus porro sint, consequuntur provident ab tempore. Atque dolorem sit numquam deleniti molestias suscipit dolores. Autem esse aperiam saepe impedit facilis reprehenderit libero nam deleniti dicta velit, alias veniam explicabo dolore iure atque eum officiis rem ratione doloribus.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
